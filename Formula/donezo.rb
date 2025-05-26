9dd6078db14a3a8d35d67b73d7392256c0ad46bf
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-26 01:54:25 -0400

    Fix formulae definitions

--- Formula/donezo.rb
+++ Formula/donezo.rb
@@ -4,19 +4,19 @@ class Donezo < Formula
   license "MIT"
   version = "0.1.0"
 
-   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-amd64.tar.gz"
-      sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
-    else
-      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-arm64.tar.gz"
-      sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
+  stable do
+    if OS.mac?
+      if Hardware::CPU.intel?
+        url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-amd64.tar.gz"
+        sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
+      else
+        url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-arm64.tar.gz"
+        sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
+      end
+    elsif OS.linux?
+      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-linux-amd64.tar.gz"
+      sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
     end
-  end
-
-  on_linux do
-    url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-linux-amd64.tar.gz"
-    sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
   end
 
   def install
@@ -24,7 +24,7 @@ class Donezo < Formula
   end
 
   test do
-    version_output = shell_output("#{bin}/donezo -version")
-    assert_match "donezo #{version}\n", version_output
+    output = shell_output("#{bin}/donezo --version")
+    assert_match "donezo #{version}", output
   end
 end
