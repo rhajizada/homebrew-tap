a4edbbef7897ca06802ecc1bf562e890ff006fe4
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-26 01:59:10 -0400

    fixing style

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -2,20 +2,19 @@ class Copycat < Formula
   desc "CLI to copy project source code as Markdown to clipboard for LLMs"
   homepage "https://github.com/rhajizada/copycat"
   license "MIT"
+  version = "0.2.0"
 
-  stable do
-    if OS.mac?
-      if Hardware::CPU.intel?
-        url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-x86_64-apple-darwin.tar.gz"
-        sha256 "5f41303343bbd9bd54d23df2118df86507605526d30034d276c7e135ddf88e46"
-      else
-        url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-aarch64-apple-darwin.tar.gz"
-        sha256 "9e5bdce56fc6ef76595241529dba5dbd7de184d7724133dba1dc53c9c73f74d4"
-      end
-    elsif OS.linux?
-      url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "75a21808868c985534b3bd1f5af3495e15a15173c7fc966a8d3b3b5076985025"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-x86_64-apple-darwin.tar.gz"
+      sha256 "5f41303343bbd9bd54d23df2118df86507605526d30034d276c7e135ddf88e46"
+    else
+      url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-aarch64-apple-darwin.tar.gz"
+      sha256 "9e5bdce56fc6ef76595241529dba5dbd7de184d7724133dba1dc53c9c73f74d4"
     end
+  else
+    url "https://github.com/rhajizada/copycat/releases/download/v0.2.0/copycat-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "75a21808868c985534b3bd1f5af3495e15a15173c7fc966a8d3b3b5076985025"
   end
 
   def install
--- Formula/donezo.rb
+++ Formula/donezo.rb
@@ -4,19 +4,17 @@ class Donezo < Formula
   license "MIT"
   version = "0.1.0"
 
-  stable do
-    if OS.mac?
-      if Hardware::CPU.intel?
-        url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-amd64.tar.gz"
-        sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
-      else
-        url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-arm64.tar.gz"
-        sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
-      end
-    elsif OS.linux?
-      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-linux-amd64.tar.gz"
-      sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-amd64.tar.gz"
+      sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
+    else
+      url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-arm64.tar.gz"
+      sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
     end
+  elsif OS.linux?
+    url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-linux-amd64.tar.gz"
+    sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
   end
 
   def install
