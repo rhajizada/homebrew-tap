aa5944128dd454310d78b83c37e7da61bfc3d4a2
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-26 03:45:49 -0400

    works locally

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -6,17 +6,14 @@ class Copycat < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-apple-darwin.tar.gz",
-          using: :nounzip
+      url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-apple-darwin.tar.gz"
       sha256 "5f41303343bbd9bd54d23df2118df86507605526d30034d276c7e135ddf88e46"
     else
-      url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-aarch64-apple-darwin.tar.gz",
-          using: :nounzip
+      url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-aarch64-apple-darwin.tar.gz"
       sha256 "9e5bdce56fc6ef76595241529dba5dbd7de184d7724133dba1dc53c9c73f74d4"
     end
   else
-    url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-unknown-linux-gnu.tar.gz",
-        using: :nounzip
+    url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "75a21808868c985534b3bd1f5af3495e15a15173c7fc966a8d3b3b5076985025"
   end
 
