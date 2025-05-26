89e9a2ff46f1ff97867710f58b5a19a5137dd621
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-26 04:21:52 -0400

    testing again

--- Formula/donezo.rb
+++ Formula/donezo.rb
@@ -1,29 +1,22 @@
 class Donezo < Formula
   desc "Simple TUI to-do app written in Go using Bubble Tea and SQLite"
   homepage "https://github.com/rhajizada/donezo"
+  version "0.1.0"
   license "MIT"
-  head "https://github.com/rhajizada/donezo.git", branch: "main"
 
-  stable do
-    version "0.1.0"
-
-    if OS.mac?
-      if Hardware::CPU.intel?
-        url "https://github.com/rhajizada/donezo/releases/download/v#{version}/donezo-darwin-amd64.tar.gz"
-        sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
-      else
-        url "https://github.com/rhajizada/donezo/releases/download/v#{version}/donezo-darwin-arm64.tar.gz"
-        sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
-      end
-    elsif OS.linux?
-      url "https://github.com/rhajizada/donezo/releases/download/v#{version}/donezo-linux-amd64.tar.gz"
-      sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
-    end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-amd64.tar.gz"
+    sha256 "7343b970c05bde06b984c73d48e30b972a9d930ec1fe43584abe88c9dacdecde"
   end
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-darwin-arm64.tar.gz"
+    sha256 "0b8d26077994da39389a7c1491d0b3a011e3469c078cb32d6ff99a26f82a9869"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/rhajizada/donezo/releases/download/v0.1.0/donezo-linux-amd64.tar.gz"
+    sha256 "65ffd54223d07f75105be054aac44c745c9abd098525904230862fbc84c86c30"
   end
 
   def install
