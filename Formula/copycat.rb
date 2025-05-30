39c05290995bd2db9aef32e7191aab838d1106a7
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-05-29 20:33:13 -0400

    Updating `copycat` to `0.2.2`

--- Formula/copycat.rb
+++ Formula/copycat.rb
@@ -1,22 +1,22 @@
 class Copycat < Formula
   desc "CLI to copy project source code as Markdown to clipboard for LLMs"
   homepage "https://github.com/rhajizada/copycat"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-apple-darwin.tar.gz"
-    sha256 "e9c9ecffefb701d7f582b08222b53fbef6838ccfe099d05fe989545e966097ec"
+    sha256 "91eb679d0ab2b888109d277812ca6a63bb79c2cdc0467b35afdf211626fa95ca"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-aarch64-apple-darwin.tar.gz"
-    sha256 "2b690914a1bbb68a98183a710ba2311d2f2f3ff3be4b0fece4a38259b3d59bd5"
+    sha256 "6af79e8cd7d0fa4fc63c706cafa6a3d4bee0b1537eb0898c43599e9f884d562e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/rhajizada/copycat/releases/download/v#{version}/copycat-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "a74efa364c318fb980f7c70c79cf196d55f529dc3ac363337949d43e53da83b0"
+    sha256 "75a21808868c985534b3bd1f5af3495e15a15173c7fc966a8d3b3b5076985025"
   end
 
   def install
