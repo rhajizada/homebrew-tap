0171ea46413bc01f98e6bc595e26110feb36a827
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-12-10 23:16:00 -0500

    donezo 0.1.1 (#9)

--- Formula/donezo.rb
+++ Formula/donezo.rb
@@ -1,8 +1,8 @@
 class Donezo < Formula
   desc "Simple TUI to-do app written in Go using Bubble Tea and SQLite"
   homepage "https://github.com/rhajizada/donezo"
-  url "https://github.com/rhajizada/donezo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4c52bb43338ef98d074437c60dd91e5d633bb627d6bf4a044dc1689dfbcc5430"
+  url "https://github.com/rhajizada/donezo/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "acc352f3ea2d10865dc40fbcdae94af1437b2aeac21337839e175b790c83d36b"
   license "MIT"
   head "https://github.com/rhajizada/donezo.git", branch: "main"
 
@@ -21,11 +21,10 @@ class Donezo < Formula
   end
 
   bottle do
-    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/donezo-0.1.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41ad303ea1b0ae12a144292ac7eb0eabf1cddd9fadc54418bafd0c3d46026a3d"
-    sha256 cellar: :any_skip_relocation, ventura:       "dd2cbe8f052913f4e2d647106b9119105bf5f9b3030a09f0ce6a1ae0f98d35a0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "378ae7c96365b6d119af4d86225bb656bf0298d3f3a2c77b0b28be7eb5781d3a"
+    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/donezo-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7f22627fc873d62c5f62f11b2e68d5517fc0748a20debcdd9d8347b5437b1996"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "0f860c580f0af1cc0e6d02d717c28856f52cbf910ab6ed741d93e3fa8fd4ff3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8016c17267b941ae8a60613927abf52a27d81666424af5f3ddb78da7f3a8ef4c"
   end
 
   depends_on "go" => :build
