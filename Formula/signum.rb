fd7da2ec06399b9a40e3c31849501c31c98f72d3
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-01-03 22:30:44 -0500

    adding formula for signum

---
+++ Formula/signum.rb
@@ -0,0 +1,38 @@
+class Signum < Formula
+  desc "SVG badge generator written in Go."
+  homepage "https://github.com/rhajizada/signum"
+  url "https://github.com/rhajizada/signum/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "33161d6d4367a4f6ae8c96ffca2e136b532b9b60f1bf9565212c83c281852a36"
+  license "MIT"
+  head "https://github.com/rhajizada/signum.git", branch: "main"
+
+  livecheck do
+    url     :stable
+    regex(/^v(\d+\.\d+\.\d+)$/i)
+
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+
+        match = release["tag_name"]&.match(regex)
+        match[1] if match
+      end
+    end
+  end
+
+  bottle do
+    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4fb6092ca2492069ab712221e7c462ef57d4cee5d2b7b11628f99fcbb8e16906"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match "signum #{version}", shell_output("#{bin}/signum --version")
+  end
+end
