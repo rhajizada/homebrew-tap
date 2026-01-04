fd29a74e3109ad5f6d9967799626346d7983e7b9
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-01-03 22:45:43 -0500

    fixing signum recipe

--- Formula/signum.rb
+++ Formula/signum.rb
@@ -22,6 +22,7 @@ class Signum < Formula
 
   bottle do
     root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "36da72cb0f70e4fbdec693760d4f38c66c415c1ab3d495d2347683db75e57a1e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4fb6092ca2492069ab712221e7c462ef57d4cee5d2b7b11628f99fcbb8e16906"
   end
 
@@ -29,7 +30,7 @@ class Signum < Formula
 
   def install
     ldflags = "-s -w -X main.Version=#{version}"
-    system "go", "build", *std_go_args(ldflags:)
+    system "go", "build", *std_go_args(ldflags:), "./cmd/cli"
   end
 
   test do
