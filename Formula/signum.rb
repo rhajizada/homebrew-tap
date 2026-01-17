0314459b203c59ec29562ecdae16da3b6f3bb4bb
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-01-17 00:56:07 -0500

    fixing formula

--- Formula/signum.rb
+++ Formula/signum.rb
@@ -2,7 +2,7 @@ class Signum < Formula
   desc "SVG badge generator written in Go"
   homepage "https://github.com/rhajizada/signum"
   url "https://github.com/rhajizada/signum/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "bc0f9992106290fde8eab3555e9ef53bf2701a302dfba9df0fdcd76145ddacfa"
+  sha256 "a073378f312030449ce579a4c53f2eb06847d3fcaf6c86994b8c2c21181dd9e4"
   license "MIT"
   head "https://github.com/rhajizada/signum.git", branch: "main"
 
@@ -21,10 +21,7 @@ class Signum < Formula
   end
 
   bottle do
-    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "3fe8cd9410b98aef11def2ba2c09dfa2418336be850104bfbd5842a855fdf8a4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "95baffb2b98422ef4a16f17f8085e7cf382f14a494dfe89d1e945b0bddb815b5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "cd09622bc365cd6a50d1b48a57107d57a8e57ef844bd29ba66da5ee6225a6080"
+    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.1"
   end
 
   depends_on "go" => :build
