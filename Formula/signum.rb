482ae48477f44d69003a04ce5edac8a8763ae65b
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-01-17 01:01:40 -0500

    returning bottles

--- Formula/signum.rb
+++ Formula/signum.rb
@@ -21,7 +21,10 @@ class Signum < Formula
   end
 
   bottle do
-    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.1"
+    root_url "https://github.com/rhajizada/homebrew-tap/releases/download/signum-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "3fe8cd9410b98aef11def2ba2c09dfa2418336be850104bfbd5842a855fdf8a4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "95baffb2b98422ef4a16f17f8085e7cf382f14a494dfe89d1e945b0bddb815b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cd09622bc365cd6a50d1b48a57107d57a8e57ef844bd29ba66da5ee6225a6080"
   end
 
   depends_on "go" => :build
